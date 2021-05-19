f1ce93b0d6243aa069b964d847535ea8e63a5c0d
definity-smileY <yysdntjq@naver.com>
2021-05-20 00:07:44 +0900

    add settings and main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# lists Common and Mutable sequence operations 
+days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+print(type(days))
+# Tuples implement all of the common sequence operation
+days = ("Mon", "Tue", "Wed", "Thur", "Fri")
+# dictionary
+nico = {
+    "name" :"Nico",
+    "age" : 29,
+    "korean": True,
+    "fav_food" : ["Kimchi", "Sashimi"]
+}
+
+print(nico)
+nico["hadnsome"] = True
+print(nico)
+
+# Todo Function
